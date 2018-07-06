e7459974a6c0d78d68a562a342cb1cd9d7439e3a
erikhaandrikman <e.haandrikman@metrological.com>
2018-07-06 14:41:27 +0200

    update(Menu): add categories

--- js/src/VodMain.js
+++ js/src/VodMain.js
@@ -2,27 +2,33 @@ class VodMain extends wuf.Application {
 
     static _template() {
         return {
-            Background: {type: EpicBackground},
-            Loader: {rect: true, w: 50, h: 50, x: 960, y: 540, pivot: 0.5, alpha: 0}
+            Background: { type: EpicBackground  },
+            Loader: {
+                 w:150, h:150, scale: 0.5, pivot: 0.5, mount:0.5, x:960, y:540, alpha: 0, src:'images/vod-loader.png'
+            },
+            Menu:{ type:VodMenu, visible: false }
         }
     }
 
     static _states() {
         return {
             _init: function() {
-                this._loadingAnimation = this.tag("Loader").animation({duration: 1, repeat: -1, actions: [
-                    {p: 'rotation', v: {sm: 0, 0: 0, 1: 2 * Math.PI}}
+                this._loadingAnimation = this.tag("Loader").animation({duration: 1.5, repeat: -1, actions: [
+                    {p: 'rotation', v: {sm: 0, 0: 0, 1: 2 * Math.PI}},
+                    {p: 'scale', v: {0: 0.3, 0.5: 0.6, 1:0.3}},
+                    {p: 'alpha', v: {0: 0, 0.5: 1, 1:0}},
                 ]})
-
                 setTimeout(() => {
                     const categories = [
                         {name: "Popular"},
-                        {name: "Drama"},
                         {name: "Action"},
-                        {name: "Sci-Fi"}
+                        {name: "Adventure"},
+                        {name: "Animation"},
+                        {name: "Comedy"},
                     ]
                     this.fire('loaded', {categories})
                 }, 2000)
+
                 return "Loading"
             },
             Loading: {
@@ -37,7 +43,10 @@ class VodMain extends wuf.Application {
                 loaded: "Loaded"
             },
             Loaded: {
-
+                _enter: function({args:{categories}}){
+                    this.tag('Menu').items = categories
+                    this.tag('Menu').visible = true
+                }
             }
         }
     }
--- js/src/VodMenu.js
+++ js/src/VodMenu.js
@@ -1,11 +1,63 @@
 class VodMenu extends wuf.Component {
-
     static _template() {
-        return {}
+        return {
+            Wrapper:{
+                alpha:0, y:-200,
+                mountX: 0.5, x: 960, y: 100,
+                texture: wuf.Tools.getRoundRect(1700,100,20, 0, 0x00000000, true, 0x33ffffff),
+                Items:{
+                    y: 25, x:25
+                }
+            }
+        }
+    }
+
+    set items(v){
+        this._items = v
+
+        this.tag('Items').patch({
+            children:v.map((item)=>{
+                return {type:MenuItem, label:item.name}
+            })
+        })
+
+        let pos = 0
+
+        this.tag('Items').children.forEach((item)=>{
+            item.x = pos
+            item.loadTexture()
+            pos+=item.renderWidth + MenuItem.MARGIN
+        })
     }
 
     static _states() {
-        return {}
-    }
+        return {
+            _active: function(){
+                this.patch({
+                    Wrapper:{
+                        smooth:{alpha:1, y:50}
+                    }
+                })
+            },
+            _inactive: function(){
 
+            }
+        }
+    }
 }
+
+class MenuItem extends wuf.Component{
+    static _template(){
+        return {
+            text:'', fontSize:40
+        }
+    }
+    set label(v){
+        this.patch({
+            text:{text:v}
+        })
+    }
+}
+
+MenuItem.MARGIN = 50
+
